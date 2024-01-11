e32a5840fbfdb168fdf9f6d50aced547f08ac641
Théo Lecolley <TheoDev@Air-de-Theo.lan>
2024-01-11 11:04:28 +0100

    test

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const sharp = require("sharp");
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.post("/applySepia", async (req, res) => {
+  try {
+    const imageData = req.body.imageData;
+
+    // Decode base64 image data
+    const buffer = Buffer.from(imageData, "base64");
+
+    // Apply Sepia effect using Sharp
+    const modifiedImageData = await sharp(buffer)
+      .tint('#704214') // Apply a brownish tint for sepia effect
+      .toBuffer();
+
+    // Save the modified image to a file (optional)
+    fs.writeFileSync("modifiedImage.jpg", modifiedImageData);
+
+    // Send the modified image data in the response
+    res.set("Content-Type", "image/jpeg");
+    res.set("Content-Disposition", "attachment; filename=modifiedImage.jpg");
+    res.send(modifiedImageData);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
+
+app.post("/applyBlackAndWhite", async (req, res) => {
+  try {
+    const imageData = req.body.imageData;
+
+    // Decode base64 image data
+    const buffer = Buffer.from(imageData, "base64");
+
+    // Apply Sepia effect using Sharp
+    const modifiedImageData = await sharp(buffer)
+      .greyscale() // Apply a brownish tint for sepia effect
+      .toBuffer();
+
+    // Save the modified image to a file (optional)
+    fs.writeFileSync("modifiedImage.jpg", modifiedImageData);
+
+    // Send the modified image data in the response
+    res.set("Content-Type", "image/jpeg");
+    res.set("Content-Disposition", "attachment; filename=modifiedImage.jpg");
+    res.send(modifiedImageData);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
