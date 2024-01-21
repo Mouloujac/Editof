4185d43b19109ff528dc0efc40723f77a3c6c4ec
Théo Lecolley <TheoDev@Air-de-Theo.lan>
2024-01-21 16:44:32 +0100

    app working

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.post("/applySepia", async (req, res) => {
   try {
     const imageData = req.body.imageData;
+    console.log(typeof imageData)
+    
     const mirrorX = req.body.mirrorX;
     const mirrorY = req.body.mirrorY;
     // Decode base64 image data
@@ -75,17 +77,35 @@ app.post("/applySepia", async (req, res) => {
 app.post("/applyBlackAndWhite", async (req, res) => {
   try {
     const imageData = req.body.imageData;
-
+    const mirrorX = req.body.mirrorX;
+    const mirrorY = req.body.mirrorY;
     // Decode base64 image data
     const buffer = Buffer.from(imageData, "base64");
-
+    let modifiedImageData
     // Apply Sepia effect using Sharp
-    const modifiedImageData = await sharp(buffer)
+    if (mirrorX === "1" && mirrorY === "0"){
+      modifiedImageData = await sharp(buffer)
+      .greyscale() // Apply a brownish tint for sepia effect
+      .flip()
+      .toBuffer();
+    }else if(mirrorY === "1" && mirrorX === "0"){
+      modifiedImageData = await sharp(buffer)
+      .greyscale() // Apply a brownish tint for sepia effect
+      .flop()
+      .toBuffer();
+    }else if(mirrorY === "1" && mirrorX === "1"){
+      modifiedImageData = await sharp(buffer)
+      .greyscale() // Apply a brownish tint for sepia effect
+      .flip()
+      .flop()
+      .toBuffer();
+    }else{
+      modifiedImageData = await sharp(buffer)
       .greyscale() // Apply a brownish tint for sepia effect
       .toBuffer();
-
+    }
     // Save the modified image to a file (optional)
-    
+ 
 
     // Send the modified image data in the response
     res.set("Content-Type", "image/jpeg");
@@ -96,6 +116,46 @@ app.post("/applyBlackAndWhite", async (req, res) => {
     res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 });
+
+app.post("/applyJustMirroring", async (req, res) => {
+  try {
+    const imageData = req.body.imageData;
+    const mirrorX = req.body.mirrorX;
+    const mirrorY = req.body.mirrorY;
+   
+    // Decode base64 image data
+    const buffer = Buffer.from(imageData, "base64");
+
+    let modifiedImageData;
+    // Apply mirroring effect using Sharp
+    if (mirrorX === "1" && mirrorY === "0"){
+      modifiedImageData = await sharp(buffer)
+        .flip()
+        .toBuffer();
+    } else if(mirrorY === "1" && mirrorX === "0"){
+      modifiedImageData = await sharp(buffer)
+        .flop()
+        .toBuffer();
+    } else if(mirrorY === "1" && mirrorX === "1"){
+      modifiedImageData = await sharp(buffer)
+        .flip()
+        .flop()
+        .toBuffer();
+    } else {
+      modifiedImageData = await sharp(buffer)
+        .toBuffer();
+    }
+    
+    // Send the modified image data in the response
+    res.set("Content-Type", "image/jpeg");
+    res.set("Content-Disposition", "attachment; filename=modifiedImage.jpg");
+    res.send(modifiedImageData);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
