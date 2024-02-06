730f3adefe64164277e7b89b2bfae13ce9dfd90c
Théo Lecolley <TheoDev@Air-de-Theo.lan>
2024-02-06 14:33:46 +0100

    final version

--- netlify/functions/applySepia.js
+++ netlify/functions/applySepia.js
@@ -5,7 +5,7 @@ const sharp = require("sharp");
 exports.handler = async (event, req, context) => {
   try {
     if (!event.body) {
-      throw new Error("Le corps de la requête est vide."+event.imageData);
+      throw new Error("Le corps de la requête est vide."+req.body+event.body);
     }
 
     const { imageData, mirrorX, mirrorY } = JSON.parse(event.body);
