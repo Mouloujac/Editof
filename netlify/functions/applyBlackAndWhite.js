d3a8f53c27b83e9c703b3e90b95a2ebc1b1f102a
Théo Lecolley <theolecolley@gmail.com>
2024-02-12 19:41:54 +0100

    test

--- netlify/functions/applyBlackAndWhite.js
+++ netlify/functions/applyBlackAndWhite.js
@@ -30,7 +30,7 @@ function handleGet(event) {
 
 async function handlePost(event) {
   // Vérifier si le corps de la requête est vide
-  console.log("post passé")
+  console.log("post")
   if (!event.body) {
     return {
       statusCode: 400,
